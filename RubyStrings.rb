c3c601b542fad1e5e8d4a36c041203afc44727bc
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-16 09:50:55 -0500

    adds Strings practice file

---
+++ RubyStrings.rb
@@ -0,0 +1,14 @@
+#String Concatenation
+sentence = "My name is Bruce"
+p sentence
+
+# With single quotes, you wont be able to do string interpolation
+
+
+#String concatenation
+first_name="M."
+last_name="Ward"
+puts first_name + " " + last_name
+
+#String Interpolation- MUST USE DOUBLE QUOTES
+puts "My first name is  #{first_name} and my last name is #{last_name}."
