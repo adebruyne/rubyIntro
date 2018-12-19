1bb44c305e5778c8e9fc7460d7728809c4ecd5f8
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-19 11:41:02 -0500

    includes functionality of crud module into student classs

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -7,15 +7,15 @@ module Crud
 
     
     # CREATE A HASH FOR USER PASSWORD
-    def self.create_hash_digest(password)
+    def create_hash_digest(password)
         BCrypt::Password.create(password)
     end
     
-    def self.verify_hash_digest(password)
+    def verify_hash_digest(password)
         BCrypt::Password.new(password)
     end
     
-    def self.create_secure_users(list_of_users)
+    def create_secure_users(list_of_users)
         list_of_users.each do |user_record|
             user_record[:password] = create_hash_digest(user_record[:password])
         end
@@ -26,7 +26,7 @@ module Crud
     # puts new_users
 
     # AUTHENTICATE USER
-    def self.authenticate_user(username, password, list_of_users)
+    def authenticate_user(username, password, list_of_users)
         # for each user, check to see if the username and the verified hashed pw match
         list_of_users.each do |user_record|
             if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,4 +1,8 @@
+require_relative 'crud'
+
+
 class Student
+    include Crud 
     attr_accessor :first_name, :last_name, :email, :username, :password
     
     def initialize(first_name,last_name,username,email,password)
@@ -19,9 +23,15 @@ end
 aylin = Student.new("Stink", "McGee", "Pewpew1", "stinkmcgee@gmail.com", "password1")
 brian = Student.new("Link", "Bingo", "Lewlew1", "linkmcgee@gmail.com", "password2")
 
-puts aylin
-puts brian
 
-aylin.last_name =brian.last_name
+aylin_hashed_pw = aylin.create_hash_digest(aylin.password)
 
-puts aylin
+puts aylin_hashed_pw
+
+
+# puts aylin
+# puts brian
+
+# aylin.last_name =brian.last_name
+
+# puts aylin
