9684be3fd24198d10b53f7d9ee14b12adc1c58c7
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-19 11:31:08 -0500

    change methods to reference self instead of module name

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -1,43 +1,42 @@
-require 'bcrypt'
- 
-users = [
-          { username: "mashrur", password: "password1" },
-          { username: "jack", password: "password2" },
-          { username: "arya", password: "password3" },
-          { username: "jonshow", password: "password4" },
-          { username: "heisenberg", password: "password5" }
-        ]
- 
-# CREATE A HASH FOR USER PASSWORD
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
- 
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
- 
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
-  end
-  list_of_users
-end
+module Crud
 
-new_users = create_secure_users(users)
-puts new_users
 
-# AUTHENTICATE USER
-def authenticate_user(username, password, list_of_users)
-    # for each user, check to see if the username and the verified hashed pw match
-    list_of_users.each do |user_record|
-        if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-            return user_record
-        end
+    require 'bcrypt'
+    puts "Module crud activated"
+    
 
+    
+    # CREATE A HASH FOR USER PASSWORD
+    def self.create_hash_digest(password)
+        BCrypt::Password.create(password)
     end
-    "Credentials were not correct."
+    
+    def self.verify_hash_digest(password)
+        BCrypt::Password.new(password)
+    end
+    
+    def self.create_secure_users(list_of_users)
+        list_of_users.each do |user_record|
+            user_record[:password] = create_hash_digest(user_record[:password])
+        end
+        list_of_users
+    end
+
+    # new_users = create_secure_users(users)
+    # puts new_users
+
+    # AUTHENTICATE USER
+    def self.authenticate_user(username, password, list_of_users)
+        # for each user, check to see if the username and the verified hashed pw match
+        list_of_users.each do |user_record|
+            if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+                return user_record
+            end
+
+        end
+        "Credentials were not correct."
+    end
+
 end
 
-
-p authenticate_user("heisenberg", "password5", new_users)
+# p authenticate_user("heisenberg", "password5", new_users)
